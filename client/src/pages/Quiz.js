d2bd8e171bffbc1fe6af96f6cfd18447caedd412
Kyle Berner <kyleberner14@yahoo.com>
2020-05-19 10:52:48 -0700

    Created a basic quizpage layout

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -1,9 +1,46 @@
 import React from "react";
+import "../pages/Quiz.css";
 
-function Quiz(){
-  return(
-      <div> Dunk or Flunk! Click to begin! </div>
-  )  
+function Quiz() {
+  return (
+    <div className="uk-cover-container uk-height-viewport" id="quizContainer">
+      <h1 className="uk-heading-medium uk-text-center" id="header"><span>Who would win at 1 on 1?</span></h1>
+      <hr className="uk-divider-icon"></hr>
+      <div className="uk-flex uk-flex-center" id="centerQuiz">
+        <div className="uk-card uk-card-hover uk-width-1-3@m">
+          <div className="uk-card-header">
+            <div className="uk-grid-small uk-flex-middle" uk-grid>
+              <div className="uk-width-auto">
+                <img className="uk-border-circle" width="auto" height="auto" src="https://www.sportsnet.ca/wp-content/uploads/2013/02/jordan_michael640.jpg" />
+              </div>
+            </div>
+          </div>
+          <div className="uk-card-footer">
+            <h3>Michael Jordan AKA the GOAT</h3>
+          </div>
+        </div>
+        <div className="PvP">
+
+          <h1>Score: Your Score</h1>
+          <h2>Time: 0:20</h2>
+        </div>
+        <div className="uk-card uk-card-hover uk-width-1-3@m">
+          <div className="uk-card-header">
+            <div className="uk-grid-small uk-flex-middle" uk-grid>
+              <div className="uk-width-auto">
+                <img className="uk-border-circle" width="auto" height="auto" src="https://nba.nbcsports.com/wp-content/uploads/sites/12/2014/03/a96a6957f860e7cdd5dc48706628cd94.jpg?w=610&h=343&crop=1" />
+              </div>
+            </div>
+          </div>
+          <div className="uk-card-footer">
+            <h3>LeBron James AKA King James</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  )
 }
 
 
