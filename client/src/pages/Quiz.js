4c499d20c51b470a9e17cacc460dbb6302cbc463
Kyle Berner <kyleberner14@yahoo.com>
2020-05-19 11:36:25 -0700

    fixed univeral white text

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -3,8 +3,8 @@ import "../pages/Quiz.css";
 
 function Quiz() {
   return (
-    <div className="uk-cover-container uk-height-viewport" id="quizContainer">
-      <h1 className="uk-heading-medium uk-text-center" id="header"><span>Who would win at 1 on 1?</span></h1>
+    <div className="uk-cover-container uk-height-viewport Quiz" id="quizContainer">
+      <h1 className="uk-heading-medium uk-text-center white" id="header"><span>Who would win at 1 on 1?</span></h1>
       <hr className="uk-divider-icon"></hr>
       <div className="uk-flex uk-flex-center" id="centerQuiz">
         <div className="uk-card uk-card-hover uk-width-1-3@m">
@@ -15,14 +15,14 @@ function Quiz() {
               </div>
             </div>
           </div>
-          <div className="uk-card-footer">
-            <h3>Michael Jordan AKA the GOAT</h3>
+          <div className="uk-card-footer white">
+            <h3 className="white">Michael Jordan AKA the GOAT</h3>
           </div>
         </div>
         <div className="PvP">
 
-          <h1>Score: Your Score</h1>
-          <h2>Time: 0:20</h2>
+          <h1 className="white">Score: Your Score</h1>
+          <h2 className="white">Time: 0:20</h2>
         </div>
         <div className="uk-card uk-card-hover uk-width-1-3@m">
           <div className="uk-card-header">
@@ -33,7 +33,7 @@ function Quiz() {
             </div>
           </div>
           <div className="uk-card-footer">
-            <h3>LeBron James AKA King James</h3>
+            <h3 className="white">LeBron James AKA King James</h3>
           </div>
         </div>
       </div>
