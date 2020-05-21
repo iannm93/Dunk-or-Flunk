3de53a456c989c0cb65d64ab734f6d02b26971e0
Ian Harris <ianmharris93@gmail.com>
2020-05-21 15:21:06 -0700

    changed API call to be just 2 players at a time , changed players array to quiz.js and made changes to the default state

--- client/src/components/quizComponents/Score.js
+++ client/src/components/quizComponents/Score.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { PromiseProvider } from "mongoose";
 
-function Score() {
+function Score(props) {
   // Access state and dispatch and cause this Count component to be re-rendered
   // any time the global count state changes. The dispatch function provided
   // by the context allows this component to update the global state.
 
   return (
     <div>
-      <h2 className="white">Score: 15</h2>
+      <h2 className="white">score: {props.score}</h2>
     </div>
   );
 }
