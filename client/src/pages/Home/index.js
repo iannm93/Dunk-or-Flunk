398f68246cea17660d9c63201b69abaf88dd8cd3
Keaton Smith <keatonpsmith55@gmail.com>
2020-05-24 22:48:39 -0700

    home page logo

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,6 +3,8 @@ import { useHistory } from "react-router-dom";
 import logo from "./logo.svg";
 import "./home.css";
 import { useAuth } from "../../utils/auth";
+import Logo from "../logo.png"
+import { Link, Redirect} from "react-router-dom";
 
 function Home() {
   const { user, logout } = useAuth();
@@ -12,10 +14,17 @@ function Home() {
 
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome {user.username}</h2>
-      </div>
+        <p>
+          <a id="logout"  onClick={() => logout()}>Log Out</a>
+          <span>
+            <p>
+          <Link id="gotoprofile" >Profile</Link>
+            </p>
+          </span>
+        </p>
+        <img id="home-logo" src={Logo}></img>
+        <h2>Welcome, {user.username}!</h2>
+      
       <p className="App-intro">
         <button
           type="button"
