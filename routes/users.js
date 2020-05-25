1b41ac81cf193c5e12a9936d826e57437d92df9d
Kyle Berner <kyleberner14@yahoo.com>
2020-05-25 10:15:45 -0700

    all routing and functionality complete

--- routes/users.js
+++ routes/users.js
@@ -17,4 +17,22 @@ router.get("/api/user/:id", isAuthenticated, (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+router.get("/api/users/", (req, res) => {
+  db.User.find({})
+    .then((users) => res.json(users))
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
+
+router.patch("/api/users/:id", (req, res) => {
+  db.User.updateOne({ _id: req.params.id }, req.body)
+    .then((highScore) => console.log(highScore))
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
