49623e34e7e8e2fb3d1f36c6ae544625809f12de
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-06-02 18:08:44 -0700

    added in bootstrap for endquiz page

--- client/src/pages/EndQuiz/EndQuiz.js
+++ client/src/pages/EndQuiz/EndQuiz.js
@@ -68,8 +68,15 @@ function EndQuiz(props) {
 
       </div>
 
+    
+    <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Fluid jumbotron</h1>
+      <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
+  </div>
 
+    </div>
 
   );
 }
